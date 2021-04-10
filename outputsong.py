c38002402e5038a28eba7f6cc3c07e0e8d5cecd4
62fe50 <merrickslane@gmail.com>
2021-04-10 11:59:54 -0700

    fixed port error

--- outputsong.py
+++ outputsong.py
@@ -1,11 +1,11 @@
 import mido
+import time
 
 def output_song(newSong):
-    
-    outport = mido.open_output(mido.get_output_names()[0])
-    print(outport)
 
-    with mido.open_output(mido.get_output_names()[0]) as outport:
+    with mido.open_output() as outport:
         for note in newSong:
             print(note)
+            outport.send(note)
+            
     outport.close()
