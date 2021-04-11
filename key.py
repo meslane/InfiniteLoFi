70bce35a9fdbfe0d7516d27e5dffea7de60694be
ckwin001 <ckwin001@ucr.edu>
2021-04-10 20:52:03 -0700

    Black Keys

--- key.py
+++ key.py
@@ -6,31 +6,37 @@ class piano():
 
     def build(self, screen):
         num_keys = 88
-        self.add(0, 1, screen)
-        self.add(1, 2, screen)
-        self.add(0, 3, screen)
+        self.add(0, 0, screen)
+        self.add(1, 1, screen)
+        self.add(0, 2, screen)
         for key in range(1, num_keys-3):
             if(key % 12 == 2 or key % 12 == 4 or key % 12 == 7
             or key % 12 == 9 or key % 12 == 11):
                 self.add(1, key, screen)
             else:
                 self.add(0, key, screen)
-        self.add(0, 88, screen)
+        self.add(0, 87, screen)
 
     def add(self, color, number, screen):
         #white key
+        white_width = (screen.get_width()/52)-2
         if color == 0:
-            self.keylist.append(key((number*screen.get_width()/52), 0, (screen.get_width()/52)-2, screen.get_height()/4 - 2, 0, (255, 155, 40)))
+            self.keylist.append(key((number*screen.get_width()/52), 0, white_width, screen.get_height()/4 - 2, 0, (255, 155, 40)))
         if color == 1:
-            pass
-        #     black_width = screen.get_width()/78-2
-        #
-        #     self.keylist.append(key(, 0, black_width, screen.get_height()/8 - 2, 0, (255, 155, 40))
+            black_width = screen.get_width()/78-2
+            black_X = self.keylist[number - 1].left + (white_width / 2) + 1
+            self.keylist.append(key(black_X, 0, black_width, screen.get_height()/8 - 2, 0, (255, 155, 40)))
 
     def draw(self, screen):
+        white_width = (screen.get_width() / 52) - 2
         for i in range(len(self.keylist)):
             if self.keylist[i].type == 0:
-                self.keylist[i].move_and_rescale(i*screen.get_width()/52, 0,(screen.get_width()/52)-2, screen.get_height()/4 - 2)
+                self.keylist[i].move_and_rescale(i*screen.get_width()/52, 0,white_width, screen.get_height()/4 - 2)
+
+            if self.keylist[i].type == 1:
+                black_X = self.keylist[i - 1].left + (white_width / 2) + 1
+                black_width = screen.get_width() / 78 - 2
+                self.keylist[i].move_and_rescale(black_X, 0, black_width, screen.get_height()/8 - 2)
 
             self.keylist[i].draw(screen)
 
