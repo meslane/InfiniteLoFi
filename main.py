f59cf3ec76fd47aa8feced1f9ec9c83d172762d2
akuma084 <ak057024@gmail.com>
2021-04-10 18:24:22 -0700

    Changes by casey

--- main.py
+++ main.py
@@ -5,32 +5,35 @@ import FileOpener
 import markov
 from TupleToMessage import TupleToMessage
 from outputsong import output_song
+from key import piano
 
 def main():
     print("hello Casey")
     
 if __name__ == "__main__":
     main()
+    piano = piano()
+    piano.build()
 
-    midiList = []
-    directory = 'MIDI'
-
-    files = Path(directory).glob('*')
-    for file in files:
-        timing = FileOpener.FileOpener(midiList, file)
-        #print(file)
-    
-    finalList = []
-    
-    for track in midiList:
-        finalList += track
-        
-    print(len(finalList))
-        
-    print(timing[0], timing[1])
-
-    while True:
-        m = markov.markov(finalList, 2, 500)
-        newSong = TupleToMessage(m)
-        output_song(newSong, timing[0], timing[1])
-        print("song over")
+    # midiList = []
+    # directory = 'MIDI'
+    #
+    # files = Path(directory).glob('*')
+    # for file in files:
+    #     timing = FileOpener.FileOpener(midiList, file)
+    #     #print(file)
+    #
+    # finalList = []
+    #
+    # for track in midiList:
+    #     finalList += track
+    #
+    # print(len(finalList))
+    #
+    # print(timing[0], timing[1])
+    #
+    # while True:
+    #     m = markov.markov(finalList, 2, 500)
+    #     newSong = TupleToMessage(m)
+    #     output_song(newSong, timing[0], timing[1])
+    #     print("song over")
