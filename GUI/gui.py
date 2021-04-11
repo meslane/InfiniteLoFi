a980a76723a0c292f398a80d779137bd60c0436d
akuma084 <ak057024@gmail.com>
2021-04-10 17:17:04 -0700

    Starter Gui code

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,34 +1,42 @@
-# import tkinter as tk
-#
-#
-# window = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
+import pygame
+from pygame import RESIZABLE
 
-import tkinter as tk
+pygame.init()
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-        self.geometry = "1600x1600"
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height), RESIZABLE)
+pygame.display.set_caption("Infinite LoFi Generator")
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+#initialize white key sizes
+num_white_keys = 52
+white_key_height = 100
+white_key_width = (screen_width-100)/52 - 2
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+isRunning = True
+while isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                isRunning = False
 
-root = tk.Tk()
-app = Application(master=root)
-app.master.minsize(800,600)
-app.mainloop()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+
+
+
+    screen.fill((128, 128, 128))
+    # build white keys
+    for i in range(num_white_keys):
+        curr_white_key_X = 50
+        curr_white_key_Y = screen_height - 150
+        curr_white_key = pygame.Rect(curr_white_key_X, curr_white_key_Y, white_key_width, white_key_height)
+        curr_white_key_X += white_key_width
+        pygame.draw.rect(screen, (255, 255, 255), curr_white_key)
+
+    pygame.draw.rect(screen, (0, 0, 0), (350, 200, 100, 50))
+
+    pygame.display.update()
